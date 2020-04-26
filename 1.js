a2515f8782b941037b383ec3de73f685f79caa5e
Nobi99 <chienNobi@nobi.noreply.github.com>
2020-04-26 11:17:07 +0700

    First Commit

---
+++ 1.js
@@ -0,0 +1,112 @@
+// anh xa
+var video_2_div = document.getElementsByClassName('video-2-div')[0];
+var video_2 = document.getElementsByClassName('video_2')[0];
+var thoat2 = document.getElementsByClassName('thoat')[0];
+
+var video_3_div = document.getElementsByClassName('video-2-div')[1];
+var video_3 = document.getElementsByClassName('video_2')[1];
+var thoat3 = document.getElementsByClassName('thoat')[1];
+
+var video_4_div = document.getElementsByClassName('video-2-div')[2];
+var video_4 = document.getElementsByClassName('video_2')[2];
+var thoat4 = document.getElementsByClassName('thoat')[2];
+
+var video_5_div = document.getElementsByClassName('video-2-div')[3];
+var video_5 = document.getElementsByClassName('video_2')[3];
+var thoat5 = document.getElementsByClassName('thoat')[3];
+
+var video_6_div = document.getElementsByClassName('video-2-div')[4];
+var video_6 = document.getElementsByClassName('video_2')[4];
+var thoat6 = document.getElementsByClassName('thoat')[4];
+
+var video_7_div = document.getElementsByClassName('video-2-div')[5];
+var video_7 = document.getElementsByClassName('video_2')[5];
+var thoat7 = document.getElementsByClassName('thoat')[5];
+
+var video_8_div = document.getElementsByClassName('video-2-div')[6];
+var video_8 = document.getElementsByClassName('video_2')[6];
+var thoat8 = document.getElementsByClassName('thoat')[6];
+
+// play video
+function show2(){
+    
+    video_2_div.classList.add('show');
+    video_2.play();
+}
+function show3(){
+    
+    video_3_div.classList.add('show');
+    video_3.play();
+}
+
+function show4(){
+    
+    video_4_div.classList.add('show');
+    video_4.play();
+}
+
+function show5(){
+    
+    video_5_div.classList.add('show');
+    video_5.play();
+}
+
+function show6(){
+    
+    video_6_div.classList.add('show');
+    video_6.play();
+}
+
+function show7(){
+    
+    video_7_div.classList.add('show');
+    video_7.play();
+}
+
+function show8(){
+    
+    video_8_div.classList.add('show');
+    video_8.play();
+}
+
+
+
+
+
+
+
+// pause video
+function unShow2(){
+    video_2_div.classList.remove('show');
+    video_2.pause();
+}
+
+function unShow3(){
+    video_3_div.classList.remove('show');
+    video_3.pause();
+}
+
+function unShow4(){
+    video_4_div.classList.remove('show');
+    video_4.pause();
+}
+
+function unShow5(){
+    video_5_div.classList.remove('show');
+    video_5.pause();
+}
+
+function unShow6(){
+    video_6_div.classList.remove('show');
+    video_6.pause();
+}
+
+function unShow7(){
+    video_7_div.classList.remove('show');
+    video_7.pause();
+}
+
+function unShow8(){
+    video_8_div.classList.remove('show');
+    video_8.pause();
+}
